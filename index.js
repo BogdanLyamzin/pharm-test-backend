8f12530a5c1ce6877cd2ca84d678ca0aeb4ded0f
MandorSavall <bogdan.lyamzin.d@gmail.com>
2020-02-26 10:54:19 +0200

    add message

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-
 const routes = require("./routes/");
 
-/*
-    app.get("/", (req, res)=> {
-        res.send("Welcome!");
+const db = require('./configs/db').mongoURI;
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+
+mongoose.connection.once('open', () => {
+    console.log('Connect to MongoDB success')
+    routes(app);
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+        console.log(`Оно живо! PORT=${PORT}`)
     });
-*/
-
-routes(app);
-
-app.listen(3000, ()=> {
-    console.log("Express work!")
 });
